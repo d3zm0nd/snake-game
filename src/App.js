80cd0a638e92c7231adc0db97fcd80db26e8f3a3
d3zm0nd <79636873+d3zm0nd@users.noreply.github.com>
2021-03-03 01:40:39 +0300

    fix: change default options

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ class App extends Component {
     this.needToChangeDrirection = '';
   }
   getInitialState = () => {
-    const borderCollisions = localStorage.getItem('borderCollisions') === 'true';
-    const selfCollisions = true; //localStorage.getItem('selfCollisions') === 'true';
+    const borderCollisions = true;//localStorage.getItem('borderCollisions') === 'true';
+    const selfCollisions = localStorage.getItem('selfCollisions') === 'true';
     const sound = localStorage.getItem('sound') === 'true';
     const countFood = parseInt(localStorage.getItem('countFood'), 10) || 1;
     const sizeArea = parseInt(localStorage.getItem('sizeArea'), 10) || DEFAULT_SIZE_AREA;
